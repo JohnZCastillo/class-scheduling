7397414cdc0f280d2f6e005af4772bc963ed5e1b
John Z Castillo <johnzunigacastillo@gmail.com>
2022-01-13 20:40:18 +0800

    version 2 and npn init

--- main.js
+++ main.js
@@ -1,150 +1,105 @@
-const Meet = require('./meet')
-const Time = require('./time')
 const Data = require('./data')
+const Time = require('./time')
 
-function mapDay(dayname){
-   const days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
-   for (let index = 0; index < days.length; index++) {
-     if(dayname ==  days[index]){
-        return index
-     }
-   }
-   return -1
+const t1 = Data.teacher('Sir John Castillo')
+const t2 = Data.teacher('Sir James Castillo')
+
+const pe = Data.subject('pe')
+const math = Data.subject('math')
+const english = Data.subject('english')
+
+
+const s1 = Data.section('section 1')
+const s2 = Data.section('section 2')
+const s3 = Data.section('section 3')
+
+const subjects = [pe,math,english]
+const sections = [s1,s2,s3]
+
+s1.subject = subjects
+s2.subject = subjects
+s3.subject = subjects
+
+const hours = Time.getTime(700,1600,30)
+const days = Time.getDays(0,4)
+
+t1.subject.push(pe,english)
+t2.subject.push(pe,english,math)
+
+t1.workingDays = Time.getDays(0,5)
+t2.workingDays = Time.getDays(0,5)
+
+t1.workingHours = hours
+t2.workingHours = hours
+
+const teachers = [t1,t2]
+
+let found = false
+
+for (const section of sections) {
+    for (const subject of subjects) {
+        
+        if(!section.hasSubject(subject)) continue
+    
+        const meeting = Data.meet(subject)
+        const meetingPool = Data.timePool(days,hours,meeting)
+
+        if(section.inSchedule(meeting)) continue
+
+        found = false
+        for (const teacher of teachers) {
+            if(found) break
+            for (const meet of meetingPool) {
+                if(!section.conflict(meet) && !teacher.conflict(meet)){
+                    if(teacher.insert(meet)){
+                        meet.teacher = teacher.name
+                        meet.section = section.name
+                        section.schedule.push(meet)
+                        found = true
+                        break
+                    }
+                }
+            }
+        }
+    }
 }
 
-// function teacher(name){
-//    return{
-//       ...Data.data(name),
-//       subjects: [],
-//       loadTracker: [], //subject day availableUnit
-//       workDays = [],
-//       totalLoad: function(){
-//          let total = 0
-//             this.subjects.forEach(subject =>{
-//                total += subject.unit
-//             })
-//          return total
-//       },
-//    }
+
+// for (const section of sections) {
+//     for(const subject of subjects){
+
+//         if(!section.hasSubject(subject)) continue
+    
+//         for (const teacher of teachers) {
+            
+//             if(!teacher.hasSubject(subject)) continue
+
+//             for (const day of days) {
+//                 for (const hour of hours) {
+                    
+//                     const meet = Data.meet(subject.name)
+//                     meet.teacher = teacher.name 
+//                     meet.section = section.name
+//                     meet.day = day
+//                     meet.start = hour
+//                     meet.setEnd(subject.units)
+//                     meet.span = subject.units
+                    
+//                     if(section.conflict(meet)) continue
+//                     if(teacher.conflict(meet)) continue
+                    
+//                     if(teacher.insert(meet)){
+//                         section.schedule.push(meet)   
+//                     }
+                    
+                  
+//                 }
+//             }
+
+
+//         }
+        
+//     }  
 // }
 
-function subject(name){
-   return{
-      ...Data.data(name),
-      unit: 0,
-      teacher: 1,
-      blockTime: [0,0,0,0,0,0,0,],//monday, tuesday... represents the subject being teacher the samte time and day
-      proceed: function(meeting){
-         //kapag ang teahcer usad lang then regualr conflict na na inverted
-         if(this.teacher <= 1){
-            console.log('proceed caled but returning conflict')
-            return !this.conflict(meeting)
-         } 
-         const day = mapDay(meeting.day);
-
-         
-         if(this.teacher > this.blockTime[day] ){
-            this.blockTime[day] += 1
-            return true
-         }
-         return false
-      }
-
-   }
-}
-
-
-const s1 = Data.data('s1')
-const s2 = Data.data('s2')
-const s3 = Data.data('s3')
-const s4 = Data.data('s4')
-const s5 = Data.data('s5')
-const s6 = Data.data('s6')
-const s7 = Data.data('s7')
-
-
-const math = subject('math')
-const english = subject('english')
-const pe = subject('pe')
-
-english.teacher = 1;
-math.teacher = 1
-pe.teacher = 1
-
-//const days =  ['monday','tuesday','wednesday','thursday','friday','saturday']
-const days =  ['monday','tuesday',]
-const hours = Time.getTime(600,1600,15)
-//const sections = [s1,s2,s3,s4,s5,s6,s7]
-const sections = [s1,s2]
-const subjects = [math,english]
-
-
-// console.log(sections[0])
-// sections.forEach(sub => {
-//    console.log(sub)
-// });
-
-sections.forEach(section =>{
-   days.forEach(day =>{
-      const breakTime = Data.data('lunchtime')
-      const lunchTime = Meet.tempMeet(breakTime,day,1100,2)
-      section.schedule.push(lunchTime)
-   })
-})
-
-console.log(hours)
-
-days.forEach(day => {
-   hours.forEach( hour =>{
-      sections.forEach(section =>{
-         subjects.forEach(subject =>{
-<<<<<<< HEAD
-            if(!section.meetingExist(subject)){//ok
-               if(!subject.meetingExist(section)){
-
-                  const subjectTemp = Meet.tempMeet(subject,day,hour,subject.unit)
-                  const sectionTemp = Meet.tempMeet(section,day,hour,subject.unit)
-
-                  //ini para maaran kung para sa anu ang meeting 
-                  subjectTemp.purpose = sectionTemp.subject
-                  sectionTemp.purpose = subjectTemp.subject
-
-=======
-            if(!section.exist(subject)){//ok
-               if(!subject.exist(section)){
-                  const subjectTemp = Meet.tempMeet(subject,day,hour,3)
-                  const sectionTemp = Meet.tempMeet(section,day,hour,3)
->>>>>>> parent of d4ac467 (stable v1: issue is no gap between subject)
-                  if(!section.conflict(subjectTemp)){
-                     if(subject.proceed(sectionTemp)){
-                         if(subjectTemp.end <= hours[hours.length-1]){
-                           section.add(subjectTemp)
-                           subject.add(sectionTemp)
-                        }
-                     }
-                  }           
-               }
-            }
-         })
-      })
-   })
-})
-
-
-
-// sections.forEach(section =>{
-//    console.log(`===========${section.name}============`)
-//   section.schedule.forEach( sched =>{
-//      if(sched.subject != 'lunchtime'){
-//         console.log(sched)
-//      }
-// })
-   
-// })
-
-subjects .forEach(sub =>{
-   console.log(`===========${sub.name}============`)
-   console.log(sub.schedule)
-})
-
-// console.log(s1.schedule)
+console.log(s3.schedule)
